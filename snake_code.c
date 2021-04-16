13708a549279fc1164d4261f55263a322eb76eea
jes550 <80514151+jes550@users.noreply.github.com>
2021-04-16 19:25:32 +0200

    Update snake_code.c

--- snake_code.c
+++ snake_code.c
@@ -1,6 +1,46 @@
 #include <stdio.h>
-#define n 20
+#include <stdlib.h>
+#include <string.h>
+#define V 30
+             	//Tamaño del campo de juego
+#define H 70
+#define N 100 	//Tamaño de la serpiente
+#define n 20 //tamaño del nombre
+typedef struct
+{
+	int x, y;
+	int ModX,ModY;
+	char imagen;
+}sprt; 			//Movimiento de la serpiente
+
+typedef struct
+{
+	int x,y;
+}frt; 			//Coordenadas de la fruta
+
+typedef struct
+{
+	char nombre[N] ;
+	int nota;
+}jug;
+
+jug jugador[N];
+sprt serpiente[N];
+frt fruta;
+void inicio(int *lon, char campo[V][H]); 				//Inicia todos los elementos
+void Intro_Campo(char campo[V][H]); 					//Dibuja el campo de juego
+void Intro_Datos(char campo[V][H], int lon); 			//Introduce los datos en la matriz del campo (serpiente y fruta)
+void input(char campo[V][H], int *lon, int *muerto); 	//Programa
+void update(char campo[V][H],int lon); 					//Borra los datos de la matriz campo y los reescribe
+void Intro_Datos2(char campo[V][H], int lon); 			//Movimiento de la serpiente
+//void loop (char campo[V][H], int lon); 					//Ejecuta draw, input y update
+char menu(void);
 int main(){
+        jug jugador[N];
+	int lon,puntos;
+	char campo[V][H];
+	char h;
+	int i=0,iMax=0;
 	int respuesta, respuesta2, respuesta3, respuesta4, salir;
 	char nombre[n];
 	
@@ -16,10 +56,10 @@ int main(){
 			case 1:
 				salir=0;
 				printf("Ha seleccionado jugar\n");
-				printf("Introduzca su nombre para empezar a jugar\n");
-				scanf("%s", nombre);
 				printf("Comienza el juego %s\n", nombre);
 				//comenzaria el juego
+				inicio(&lon, campo); 
+				loop(campo, lon);
 			break;
 		
 			case 2:
@@ -80,4 +120,90 @@ int main(){
 		}
 	}
 	while (salir==1);
+}
+//se inician todos los elementos
+void inicio(int*lon,char campo[V][H]
+{
+	int i;
+	//Cabeza de la serpiente
+	serpiente[0].x = 35;
+	serpiente[0].y = 13;
+	//Tama?
+	*lon = 4;
+	//Coordenadas aleatorias de la fruta
+	srand(time(NULL));
+	fruta.x = rand() % (H-1);
+	fruta.y = rand() % (V-1);
+	
+	while(fruta.x == 0)
+	{
+		fruta.x = rand() % (H-1);
+	}
+	while(fruta.y == 0)
+	{
+		fruta.y = rand() % (V-1);
+	}
+	for(i = 0; i < *lon; i++)
+	{
+		serpiente[i].ModX = 1;
+		serpiente[i].ModY = 0;
+	}
+	
+	Intro_Campo(campo);
+	Intro_Datos(campo, *lon);
+}
+//Creacion del campo de juego
+void Intro_Campo(char campo[V][H])
+{
+	int i, j;
+	for (i = 0; i < V; i++)
+	{
+		for (j = 0; j < H; j++)
+		{
+			if(i == 0 || i == V-1)
+			{
+				campo[i][j] = '=';
+			}
+			else if (j == 0 || j == H-1)
+			{
+				campo[i][j] = '|';
+			}
+			else
+			{
+				campo[i][j] = ' ';
+			}
+		}
+	}
+}
+
+void Intro_Datos(char campo[V][H], int tam)
+{
+	int i;
+	for (i = 1; i < tam; i++)
+	{
+		serpiente[i].x = serpiente[i-1].x - 1;
+		serpiente[i].y = serpiente[i-1].y - 1;
+		
+		serpiente[i].imagen = 'O';
+	}
+	serpiente[0].imagen = 'O';
+	
+	for(i = 0; i < tam; i++)
+	{
+		campo[serpiente[i].y][serpiente[i].x] - serpiente[i].imagen;
+	}
+	campo[fruta.y][fruta.x] - '&';
+}
+
+void draw(char campo[V][H])
+{
+	int i,j;
+	for (i = 0; i < V; i++)
+	{
+		for (j = 0; j < H; j++)
+		{
+			printf("%c", campo[i][j]);
+		}
+		printf("\n");
+	}
 }
